d72229066f2dd67bc6f412f78891a6a3ef2d144c
Patricia Nykaza <pnykaza@calstatela.edu>
2019-01-22 21:05:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const app = "I don't do much."
 
 function fatBastard(meal) {
   function whatsForDinner() {
-    if (meal === "Mini-Me") {
+    if (!meal) {
+      
+    }
+    else if (meal === "Mini-Me") {
       console.log('The wee man is in my belly!');
     } else {
       console.log(`I'm eatin' a bit of ${meal}! Burp.`);
     }
   }
   return whatsForDinner;
-
 }
